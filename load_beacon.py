3287020d52cf03f840925dc548fe8021909521dd
Mike Place <mp@saltstack.com>
2016-10-02 20:06:11 +0900

    Rename load beacon

---
+++ load_beacon.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+import zmq
+import os
+import socket
+import time
+import random
+import framer
+
+# Create a context
+ctx = zmq.Context()
+
+# Our tag
+tag = '/client/load/silver'
+while True:
+    # Frame it up
+    event = framer.pack(tag, {'cur_load': os.getloadavg()})
+    socket = ctx.socket(zmq.PUSH)
+    print('Socket connected at localhost on 2001')
+    socket.connect('tcp://localhost:2001')
+    print('Sending')
+    socket.send(event)
+    socket.close()
+    time.sleep(1)
